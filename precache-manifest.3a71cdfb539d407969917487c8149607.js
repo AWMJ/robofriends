c26999a8c6d51cff6341d4d205062b41f12d8a5f
Alvin Moats <alvinmoats2@gmail.com>
2022-10-21 13:23:53 -0400

    Updates

---
+++ precache-manifest.3a71cdfb539d407969917487c8149607.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a43172368cba32cb0974",
+    "url": "/robofriends/static/css/main.f4d1fe40.chunk.css"
+  },
+  {
+    "revision": "a43172368cba32cb0974",
+    "url": "/robofriends/static/js/main.a4317236.chunk.js"
+  },
+  {
+    "revision": "e6dbdfe8d69462beb6c8",
+    "url": "/robofriends/static/css/1.59ec7808.chunk.css"
+  },
+  {
+    "revision": "e6dbdfe8d69462beb6c8",
+    "url": "/robofriends/static/js/1.e6dbdfe8.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "0e0bf2ef643a7c83d5cadfb025250771",
+    "url": "/robofriends/index.html"
+  }
+];
